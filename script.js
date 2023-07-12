36a8841ba4386e925297bfe21d296e5602c21d61
Mtlightfoot <M-lightfoot@hotmail.com>
2023-07-12 11:44:17 +0100

    Finished my game off rock,paper and scissors. Added point system and message to let you know whos won and lose

--- script.js
+++ script.js
@@ -5,24 +5,33 @@ function getComputerChoice() {
 }
 
 function playRound() {
-    const playerSelection = "rock";
+    const playerSelection = prompt("Please select Rock, Paper or Scissors").toLowerCase();
     const computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         console.log(`It's a tie, you both picked ${playerSelection}!`);
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log(`You lose, paper beats rock!`);
+        computerScore ++;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log(`You win, rock beats scissors!`);
+        playerScore ++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log(`You win, paper beats rock!`);
+        playerScore ++;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log(`You lose, scissors beats paper!`);
+        computerScore ++;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log(`You lose, rock beats scissors!`);
+        computerScore ++;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log(`You win, scissors beats paper!`);
+        playerScore ++;
     }
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
     playRound();
@@ -30,6 +39,13 @@ function game() {
     playRound();
     playRound();
     playRound();
+    if (playerScore > computerScore) {
+        console.log("You are the winner!");
+    } else {
+        console.log("Uh oh, you lose!")
+    }
 }
 
-console.log(game());
+game();
+console.log(playerScore);
+console.log(computerScore);
